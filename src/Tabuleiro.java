82c46e656a46d437454aca19902d40f5b33bf2e1
matheus.moraes <matheus.pozzer@edu.pucrs.br>
2022-10-19 17:09:51 -0300

    Pai destruiu

--- src/Tabuleiro.java
+++ src/Tabuleiro.java
@@ -27,33 +27,19 @@ public class Tabuleiro {
             return;
         }
 
-        if (galinhas < porquinhos) {
-            if (!resolvePrimeiraClasse(tabuleiro, 0, 0, 0, 0, this.porquinhos, this.galinhas, AnimalType.GALINHA,
-                    false, tabuleiroEspacoLivre)) {
-                System.out.print("Numero solucoes: 0");
-                return;
+            if (galinhas < porquinhos) {
+                if (!resolvePrimeiraClasse(tabuleiro, 0, 0, 0, 0, this.porquinhos, this.galinhas, AnimalType.GALINHA,
+                        false, tabuleiroEspacoLivre)) {
+                    System.out.print("Numero solucoes: 0");
+                    return;
+                }
+            } else {
+                if (!resolvePrimeiraClasse(tabuleiro, 0, 0, 0, 0, this.porquinhos, this.galinhas, AnimalType.PORQUINHO,
+                        false, tabuleiroEspacoLivre)) {
+                    System.out.print("Numero solucoes: 0");
+                    return;
+                }
             }
-        } else {
-            if (!resolvePrimeiraClasse(tabuleiro, 0, 0, 0, 0, this.porquinhos, this.galinhas, AnimalType.PORQUINHO,
-                    false, tabuleiroEspacoLivre)) {
-                System.out.print("Numero solucoes: 0");
-                return;
-            }
-        }
-
-        // if (galinhas > 0) {
-        // if (!resolve(tabuleiro, 0, 0, 0, 0, this.porquinhos, this.galinhas,
-        // AnimalType.GALINHA)) {
-        // System.out.print("Numero solucoes: 0");
-        // return;
-        // }
-        // } else {
-        // if (!resolve(tabuleiro, 0, 0, 0, 0, this.porquinhos, this.galinhas,
-        // AnimalType.PORQUINHO)) {
-        // System.out.print("Numero solucoes: 0");
-        // return;
-        // }
-        // }
 
         System.out.println("Numero solucoes: " + this.numSolucoes);
     }
@@ -74,30 +60,43 @@ public class Tabuleiro {
             AnimalType animalType, boolean isEmpty, TabuleiroEspacoLivre tabuleiroEspacoLivre) {
         if (numeroGalinhas == 0 && numeroPorquinhos == 0) {
             this.numSolucoes++;
-            //boardPrinter(tabuleiro);
+            // boardPrinter(tabuleiro);
             return true;
         }
-        boardPrinter(tabuleiroEspacoLivre.tabuleiroOcupacao);
         int linha = 0;
         int coluna = 0;
         if (animalType == AnimalType.PORQUINHO) {
             coluna = colunaPorquinho;
             linha = linhaPorquinho;
+            if (tabuleiroEspacoLivre.espacosLivres < numeroGalinhas) {
+                return this.numSolucoes > 0;
+            }
         } else if (animalType == AnimalType.GALINHA) {
             linha = linhaGalinha;
             coluna = colunaGalinha;
+            if (tabuleiroEspacoLivre.espacosLivres < numeroPorquinhos) {
+                return this.numSolucoes > 0;
+            }
         }
         if (animalType == AnimalType.PORQUINHO && numeroPorquinhos == 0) {
             isEmpty = true;
             animalType = AnimalType.GALINHA;
             linha = linhaGalinha;
             coluna = colunaGalinha;
+            if (tabuleiroEspacoLivre.espacosLivres == numeroGalinhas) {
+                this.numSolucoes++;
+                return this.numSolucoes > 0;
+            }
         }
         if (animalType == AnimalType.GALINHA && numeroGalinhas == 0) {
             isEmpty = true;
             animalType = AnimalType.PORQUINHO;
             linha = linhaPorquinho;
             coluna = colunaPorquinho;
+            if (tabuleiroEspacoLivre.espacosLivres == numeroPorquinhos) {
+                this.numSolucoes++;
+                return this.numSolucoes > 0;
+            }
         }
 
         if (animalType == AnimalType.PORQUINHO) {
@@ -172,74 +171,11 @@ public class Tabuleiro {
         return this.numSolucoes > 0;
     }
 
-    private boolean resolve(int[][] tabuleiro, int linhaPorquinho, int colunaPorquinho, int linhaGalinha,
-            int colunaGalinha,
-            int numeroPorquinhos, int numeroGalinhas,
-            AnimalType animalType) {
-        if (numeroGalinhas == 0 && numeroPorquinhos == 0) {
-            this.numSolucoes++;
-            return true;
-        }
-        int linha = 0;
-        int coluna = 0;
-        if (animalType == AnimalType.PORQUINHO && numeroPorquinhos > 0) {
-            coluna = colunaPorquinho;
-            linha = linhaPorquinho;
-        } else if (animalType == AnimalType.GALINHA && numeroGalinhas > 0) {
-            linha = linhaGalinha;
-            coluna = colunaGalinha;
-        }
-
-        if (animalType == AnimalType.PORQUINHO) {
-            for (int i = linha; i < tamTabuleiro; i++) {
-                for (int j = coluna; j < tamTabuleiro; j++) {
-                    if (verificaCasa(tabuleiro, i, j, animalType)) {
-                        tabuleiro[i][j] = 2;
-                        if (numeroGalinhas > 0) {
-                            resolve(tabuleiro, i, j, linhaGalinha, colunaGalinha, numeroPorquinhos - 1,
-                                    numeroGalinhas,
-                                    AnimalType.GALINHA);
-                        } else {
-                            resolve(tabuleiro, i, j, linhaGalinha, colunaGalinha, numeroPorquinhos - 1,
-                                    numeroGalinhas,
-                                    AnimalType.PORQUINHO);
-                        }
-                        tabuleiro[i][j] = 0;
-                    }
-                    coluna = 0;
-                }
-            }
-        } else {
-            for (int i = linha; i < tamTabuleiro; i++) {
-                for (int j = coluna; j < tamTabuleiro; j++) {
-                    if (verificaCasa(tabuleiro, i, j, animalType)) {
-                        tabuleiro[i][j] = 1;
-                        if (numeroPorquinhos > 0) {
-                            resolve(tabuleiro, linhaPorquinho, colunaPorquinho, i, j, numeroPorquinhos,
-                                    numeroGalinhas - 1,
-                                    AnimalType.PORQUINHO);
-                        } else {
-                            resolve(tabuleiro, linhaPorquinho, colunaPorquinho, i, j, numeroPorquinhos,
-                                    numeroGalinhas - 1,
-                                    AnimalType.GALINHA);
-                        }
-                        tabuleiro[i][j] = 0;
-                    }
-                    coluna = 0;
-                }
-            }
-        }
-        return this.numSolucoes > 0;
-    }
-
     private boolean verificaCasa(int[][] tabuleiro, int linha, int col, AnimalType tipoAnimal) {
         int numeroChecar = 0;
-        if (tipoAnimal == AnimalType.GALINHA) {
-            numeroChecar = 2;
-        }
-        if (tipoAnimal == AnimalType.PORQUINHO) {
-            numeroChecar = 1;
-        }
+        
+        numeroChecar = tipoAnimal == AnimalType.GALINHA ? 2 : 1;
+
         int i, j;
 
         if (tabuleiro[linha][col] != 0)
@@ -371,21 +307,10 @@ public class Tabuleiro {
     public static void boardPrinter(int[][] board) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                    System.out.print(board[i][j] + " . ");
+                System.out.print(board[i][j] + " . ");
             }
             System.out.println("  ");
         }
         System.out.println("  ");
     }
-
-    // private String printaSolucao(int[][] tabuleiro) {
-    // StringBuilder mensagem = new StringBuilder();
-    // for (int i = 0; i < tamTabuleiro; i++) {
-    // for (int j = 0; j < tamTabuleiro; j++)
-    // mensagem.append(" " + tabuleiro[i][j] + " ");
-    // mensagem.append("\n");
-    // }
-    // return mensagem.toString();
-    // }
-
 }
